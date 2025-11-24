53b9b9a50db0bdb8da71483a19b0d9d4afcb845a
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-11-24 21:38:36 +0530

    inverted pyramid

---
+++ Q117.java
@@ -0,0 +1,36 @@
+/*
+Enter the Number of Rows: 5
+1  2  3  4  5  4  3  2  1  
+   2  3  4  5  4  3  2
+      3  4  5  4  3
+         4  5  4
+            5
+*/
+
+import java.util.Scanner;
+
+public class Q117 {
+    public static void printPattern(int n) {
+        int star = 2*n-1; int space = 0; int num = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=space; j++) {
+                System.out.printf("%-3s","");
+            }
+            int colMid = star/2+1;
+            for(int j=1; j<=star; j++) {
+                if(j<colMid)
+                    System.out.printf("%-3d", num++);
+                else
+                    System.out.printf("%-3d", num--);
+            }
+            star-=2; space ++; num+=2;
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Rows: ");
+        int n = sc.nextInt();
+        printPattern(n);
+    }
+}
