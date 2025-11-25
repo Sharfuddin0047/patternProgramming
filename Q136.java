774b578b7227f6556bf33b573bc2006c8b746f51
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-11-25 19:45:13 +0530

    diamond

--- Q136.java
+++ Q136.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Q136 {
     public static void printPattern(int n) {
-        int star = 1; int rowMid = n/2+1; int space = n/2; int num = (n*n)/2 - n-1;
+        int star = 1; int rowMid = n/2+1; int space = n/2; int mid = n/2+1;
+        int num = mid * mid;
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=space; j++) {
                 System.out.printf("%-2s", "");
