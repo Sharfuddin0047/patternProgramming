77c359aac42c35f2b46be43bf2831a6416d0f63d
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-12-01 19:19:31 +0530

    fibonacci number optimized

--- Recursion/Q15.java
+++ Recursion/Q15.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class Q15 {
-    public static void printFib(int n) {
+    private static long[] fibonacciCache;
+
+    public static long fib(int n) {
         if(n==0 || n==1) {
-            System.out.println(n+" ");
+            return n;
         }
-        int fib = printFib(n-1) + printFib(n-2);
+
+        if(fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
+        }
+
+        long nthfib = fib(n-1)+fib(n-2);
+        fibonacciCache[n] = nthfib;
+        return nthfib;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the nth term: ");
         int n = sc.nextInt();
-        printFib(n);
+        fibonacciCache = new long[n+1];
+        for(int i=0; i<=n; i++) {
+            System.out.print(fib(i)+" ");
+        }
+        
     }
 }
