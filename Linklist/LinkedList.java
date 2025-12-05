093796ef99ea45d983b1dc6c042c94e0c79667a4
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-12-05 23:16:43 +0530

    display

--- Linklist/LinkedList.java
+++ Linklist/LinkedList.java
@@ -16,21 +16,36 @@ class LinkedList {
 
     // Design a method to add node at the end of LinkedList.
     public void addAtEnd(int val) {
+        Node newNode = new Node(val);
         if(head == null) {
-            Node newNode = new Node(val);
+            count++;
             head = tail = newNode;
             return;
         }
-        Node newNode = new Node(val);
+        count++;
         tail.next = newNode;
         tail = newNode;
     }
 
     // Design a method to print all node values.
+    public void display(Node head) {
+        if(head == null) {
+            System.out.println(head);
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.val+"->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addAtEnd(1);
         ll.addAtEnd(2);
         ll.addAtEnd(3);
+
+        ll.display(head);
     }
 }
