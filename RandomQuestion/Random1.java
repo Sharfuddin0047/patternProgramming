0a6041d20d0d8224888b6020dd26cecc1158ef8d
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-12-08 18:47:51 +0530

    interviewPattern

--- RandomQuestion/Random1.java
+++ RandomQuestion/Random1.java
@@ -26,19 +26,19 @@ public class Random1 {
         for (int i = 1; i <= 2 * n; i++) {
             if (i <= n) {
                 for (int j = 1; j <= space; j++) {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
 
                 for (int j = 1; j <= star; j++) {
-                    System.out.print("* ");
+                    System.out.print("*   ");
                 }
             }
             if (i > n) {
                 for (int j = 1; j <= 2 * n; j++) {
                     if (i == 2 * n && j > 1 && j <= 2 * n) {
-                        System.out.print("e");
+                        System.out.print("e ");
                     } else {
-                        System.out.print(" ");
+                        System.out.print("  ");
                     }
                 }
             }
@@ -51,9 +51,9 @@ public class Random1 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 2 * n - 1; j++) {
                 if (j == n || j == ((2 * n + 1)- (n-1))) {
-                    System.out.print("@");
+                    System.out.print("@ ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
