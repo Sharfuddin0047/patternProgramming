00e75d66df232eaed1c89a3e1279f2be824f1a05
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-07 20:05:03 +0530

    String length

---
+++ Arrays/Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        String[] arr = {"Hello", "World", "Java", "Programming"};
+        for(String x: arr) {
+            System.out.println(x+" length is: "+x.length());
+        }
+    }
+}
