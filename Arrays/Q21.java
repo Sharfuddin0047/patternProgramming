d9ab498c26323b54d6acae9302ef074dd71ca32a
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-09 21:10:03 +0530

    optimized code rotate k position array

--- Arrays/Q21.java
+++ Arrays/Q21.java
@@ -7,26 +7,26 @@ Output: {6, 7, 1, 2, 3, 4, 5}
 */
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Q21 {
     public static String rotateKPosition(int[] arr, int k) {
-        int n = arr.length; int[] temp = new int[k]; int j=0;
-        for(int i=n-k;i<n; i++) { // To secure the values
-            temp[j++] = arr[i];
-        }
-        
-        for(int i=n-k-1; i>=0; i--) { //to swap
-            arr[i+2]=arr[i];
-        }
-
-        for(int i=0; i<k; i++) { // To put values back
-            arr[i] = temp[i];
-        }
+       int n=arr.length;
+       for(int i=0; i<k; i++) {
+            int last = arr[n-1];
+            for(int j=n-1; j>0; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = last;
+       }
         return Arrays.toString(arr);
     } 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
-        int k=2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of k: ");
+        int k = sc.nextInt();
         System.out.println("Rotated Array: "+rotateKPosition(arr, k));
+        sc.close();
     }
 }
