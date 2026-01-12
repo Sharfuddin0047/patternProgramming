335c870a1575c68881af16502f245542dc864aa0
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-12 22:38:38 +0530

    move zeroes

--- Arrays/Q32.java
+++ Arrays/Q32.java
@@ -5,20 +5,24 @@ o/p: [7, 2, 6, 4, 0, 0] */
 import java.util.Arrays;
 
 public class Q32 {
-    public static int[] moveZeroes(int[] arr) {
-        int[] newArr = new int[arr.length];
-        int j = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                newArr[j++] = arr[i];
+    public static void moveZeroes(int[] arr) {
+        int idx=0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != 0) {
+                arr[idx++] = arr[i];
             }
         }
-        return newArr;
+
+        while(idx<arr.length) {
+            arr[idx++]=0;
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = {7, 0, 2, 6, 0, 4};
-        int[] result = moveZeroes(arr);
-        System.out.println(Arrays.toString(result));
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
     }
+
 }
