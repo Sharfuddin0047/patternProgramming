a36e4b643e6142baa7a2eb45c681330ba1ef0461
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-13 20:49:03 +0530

    shift zeroes ones

--- Arrays/Q33.java
+++ Arrays/Q33.java
@@ -5,25 +5,28 @@ i/p: [0, 1, 1, 0, 0, 1, 0, 0]
 o/p: [0, 0, 0, 0, 0, 1, 1, 1]
 */
 
+import java.util.Arrays;
+
 public class Q33 {
-    public static void swap(int[] arr) {
-        int[] newArr=new int[arr.length];
-        int idx=0;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 0) {
-                idx++;
+    public static void shiftZeroesOnes(int[] arr) {
+        int zeroCount=0;
+        for(int x: arr) {
+            if(x==0) {
+                zeroCount++;
             }
         }
-        while(idx<arr.length){
-            newArr[idx++] = 1;
+        for(int i=0; i<arr.length; i++) {
+            if(i<zeroCount) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
         }
 
-        for(int x: newArr) {
-            System.out.print(x+" ");
-        }
+        System.out.println("updated Arrays: "+Arrays.toString(arr));
     }
     public static void main(String[] args) {
         int[] arr = {0, 1, 1, 0, 0, 1, 0, 0};
-        swap(arr);
+        shiftZeroesOnes(arr);
     }
 }
