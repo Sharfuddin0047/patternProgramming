c309a83244a645dbaa40f43bbb2575f6b4293c9d
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-19 21:04:53 +0530

    unknown range

---
+++ Arrays/Q51.java
@@ -0,0 +1,31 @@
+/*WAJP to print the frequency of each element of
+the array when elements provided are in any
+range. */
+
+public class Q51 {
+    public static void getFreq(int[] nums) {
+        int max=nums[0];
+        int min=nums[0];
+        for(int x:nums) {
+            if(max<x){
+                max=x;
+            } else if(min > x) {
+                min = x;
+            }
+        }
+
+        int[] freq = new int[max-min+1];
+        for(int x: nums) {
+            freq[x-min]++;
+        }
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i]!=0) {
+                System.out.println(i+min +" comes "+freq[i]+" times");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,23,34,34,56,47,23,43};
+        getFreq(nums);
+    }
+}
