5a34bdb8ff51eaa6b2146feb91e254266e5bd9dc
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-20 22:13:22 +0530

    even occurence

---
+++ Arrays/Q59.java
@@ -0,0 +1,31 @@
+/*WAJP to print all the elements of array whose
+frequency are even. */
+
+public class Q59 {
+    public static void duplicateElement(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+
+        for(int x:nums) {
+            if(x>max)
+                max=x;
+            else if(x<min)
+                min=x;
+        }
+
+        int[] freq=new int[max-min+1];
+        for(int x:nums) {
+            freq[x-min]++;
+        }
+
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i] !=0 && freq[i]%2==0) {
+                System.out.print((i+min)+ " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,23,34,34,56,47,47,23,43,43,56};
+        duplicateElement(nums);
+    }
+}
