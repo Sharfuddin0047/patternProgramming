76bb1f2e8887b2a7d35b75444c115ad08c53fa58
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-21 18:59:18 +0530

    maximum occurence

---
+++ Arrays/Q61.java
@@ -0,0 +1,34 @@
+/*WAJP to print the element and its frequency
+which has appeared for the maximum time in
+the array. */
+
+public class Q61 {
+    public static void maxOccurence(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+
+        for(int x:nums) {
+            if(x>max)
+                max=x;
+            else if(x<min)
+                min=x;
+        }
+
+        int[] freq=new int[max-min+1];
+        for(int x:nums) {
+            freq[x-min]++;
+        }
+
+        int maxFreq=0;
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i] > freq[maxFreq]) {
+                maxFreq=i;
+            }
+        }
+        System.out.println(min+maxFreq+" comes "+freq[maxFreq]+" times");
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,23,34,34,56,47,47,23,43,43,56};
+        maxOccurence(nums);
+    }
+}
