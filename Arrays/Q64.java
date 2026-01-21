5780ced3238d60595e5d6d2fa37251f0e544fe34
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-21 19:20:56 +0530

    biggest if equal freq

---
+++ Arrays/Q64.java
@@ -0,0 +1,30 @@
+public class Q64 {
+    public static void maxOccurence(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+
+        for(int x:nums) {
+            if(x>max)
+                max=x;
+            else if(x<min)
+                min=x;
+        }
+
+        int[] freq=new int[max-min+1];
+        for(int x:nums) {
+            freq[x-min]++;
+        }
+
+        int maxFreq=0;
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i] >= freq[maxFreq]) {
+                maxFreq=i;
+            }
+        }
+        System.out.println(min+maxFreq+" comes "+freq[maxFreq]+" times");
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,34,23,34,56,47,47,23,43,43,56};
+        maxOccurence(nums);
+    }
+}
