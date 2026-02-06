dcd66c4fa18edd07e44eb2a76156e67bdf45b693
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-02-06 22:50:32 +0530

    sort in descending order

---
+++ Arrays/Q95.java
@@ -0,0 +1,48 @@
+/*WAJP to sort the array elements in descending
+order. */
+
+import java.util.Arrays;
+
+public class Q95 {
+    public static void mergeSort(int[] nums, int start, int end) {
+        if(start<end) {
+            int mid = start+(end-start)/2;
+            mergeSort(nums, start, mid);
+            mergeSort(nums, mid+1, end);
+            merge(nums, start, mid, end);
+        }
+    }
+
+    public static void merge(int[] nums, int start, int mid, int end) {
+        int idx1=start, idx2=mid+1, idx=0;
+        int[] newArr = new int[end-start+1];
+
+        while(idx1<=mid && idx2<=end) {
+            if(nums[idx1]>=nums[idx2]) {
+                newArr[idx++] = nums[idx1++];
+            } else {
+                newArr[idx++] = nums[idx2++];
+            }
+        }
+
+        while(idx1<=mid) {
+            newArr[idx++] = nums[idx1++];
+        }
+
+        while(idx2<=end) {
+            newArr[idx++] = nums[idx2++];
+        }
+
+        for(int i=0,j=start; i<newArr.length; i++,j++) {
+            nums[j] = newArr[i];
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 9, 1, 5, 6};
+        System.out.println("Original array: " + Arrays.toString(nums));
+
+        mergeSort(nums, 0, nums.length - 1);
+
+        System.out.println("Sorted array: " + Arrays.toString(nums));
+    }
+}
